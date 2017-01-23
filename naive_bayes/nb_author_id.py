89cc86ae0681564bd38f0ea843b1291328e4fa4a
ZuzooVn <zuzoovn@gmail.com>
2017-01-23 17:48:38 +0700

    Finished Naive Bayes Session

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -4,28 +4,48 @@
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
 
     Use a Naive Bayes Classifier to identify emails by their authors
-    
+
     authors and labels:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+# Create classifier: We create an object clf which is an instance of the Naive Bayes classifier.
+clf = GaussianNB()
+
+# train
+t0 = time()
+clf.fit(features_train, labels_train)
+print "\ntraining time:", round(time() - t0, 3), "s"
+
+# predict
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time() - t0, 3), "s"
+
+# calculate and return the accuracy on the test data
+accuracy = accuracy_score(labels_test, pred)
+# accuracy = clf.score(features_test, labels_test)
+
+
+print '\naccuracy = {0}'.format(accuracy)
+
 
 
 #########################################################
